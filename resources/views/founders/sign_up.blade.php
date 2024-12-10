b7ea5501f3654db7da5781d7e2f13c7398a6322c
tena <tena.visansoft@gmail.com>
2024-12-10 14:43:01 +0530

    brief layout

--- resources/views/founders/sign_up.blade.php
+++ resources/views/founders/sign_up.blade.php
@@ -3,3 +3,13 @@
 @section('title')
 	<title>{{ config('app.name')}} | Founders Sign Up</title>
 @endsection
+
+@section('body')
+	<div class="col-6" >
+		Test
+	</div>
+	
+	
+@endsection
+
+
--- resources/views/layouts/sign_up.blade.php
+++ resources/views/layouts/sign_up.blade.php
@@ -2,9 +2,21 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+	<link href="{{ asset('public/assets/css/main.css') }}" rel="stylesheet" type="text/css" />
 	@yield('title')
 </head>
-<body>
+<body class="demo">
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				HELLO
+			</div>
+			@yield('body')
+		</div>
+	</div>
 
+	<!-- Script -->
+	<script src="{{ asset('public/assets/js/') }}"></script>
 </body>
 </html>
